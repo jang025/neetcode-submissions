27b0373ae18b595f089e4fede93cc378f5f6b568
Jeremy <92534744+jang025@users.noreply.github.com>
2026-06-19 15:00:55 +0800

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.js
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        // initial thoughts
+        // binary search only works for sorted data(sorted arrays)
+        // divide and conquer algorithm
+        // O(logn) time algo
+        // O(1) space algo
+
+        // pseudocode
+        // have left pointer pointing at the first element of the array
+        // have right pointer pointing at the last element of the array
+
+        // iterate through the array
+        // have a variable mid , point to the middle of the array
+        // check mid against the target,
+        // if its the same as the target , return mid
+        // if its less than the target , have the left pointer pointing at mid + 1
+        // if its more than the target, have the right pointer pointing at mid - 1
+
+        let leftPointer = 0;
+        let rightPointer = nums.length - 1;
+
+        while(leftPointer <= rightPointer) {
+            const mid = Math.floor((leftPointer + rightPointer) / 2);
+            if (nums[mid] < target) {
+                leftPointer = mid + 1;
+            } else if (nums[mid] > target) {
+                rightPointer = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
