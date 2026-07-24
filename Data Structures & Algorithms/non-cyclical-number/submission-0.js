192d09c8c1e0c74085eab2f40e00e48bbbe2b34e
Jeremy <92534744+jang025@users.noreply.github.com>
2026-07-24 18:19:58 +0800

    Add: non-cyclical-number - submission-0

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-0.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {boolean}
+     */
+
+
+    // helper function to process the digits 
+    sumOfSquares(n) {
+        // start at 0
+        let num = 0;
+        while(n > 0){
+            const rightMostDigit = n % 10
+            num += Math.pow(rightMostDigit , 2)
+            n = Math.floor(n / 10 )
+        }
+        return num;
+    }
+    isHappy(n) {
+        // SET data structure to store the numbers and check for duplicates
+        const mySet = new Set();
+        // start at the number n 
+        let numOutput = n;
+        while(numOutput !== 1){
+            numOutput = this.sumOfSquares(numOutput);
+            if(!mySet.has(numOutput)){
+                mySet.add(numOutput)
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
